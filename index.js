34197949511b63724579fbc5f403624094a8bb4d
Mohit <demo144@gmail.com>
2023-07-28 17:01:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 8000;
+const date = new Date();
+let pageNo = 1;
+
+// const nextPage = document.getElementsByClassName("next");
+// const previousPage = document.getElementsByClassName("previous");
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+
+
+app.get("/", async (req, res) => {
+
+    try {
+           const response = await axios.get(`https://cat-fact.herokuapp.com/facts`);
+           const result = response.data;
+
+        res.render("index.ejs", {
+            year: date.getFullYear(),
+            fact1 : result[0].text,
+            fact2 : result[1].text,
+            fact3 : result[2].text,
+            fact4 : result[3].text,
+            fact5 : result[4].text,
+        });
+
+
+    } catch(error){
+       console.log(error);
+        }
+});
+
+app.get("/nextPage",async (req, res) =>{
+
+    try {
+        pageNo += 1;
+        const response = await axios.get(`https://catfact.ninja/facts?page=${pageNo}`);
+        const nextPageresult = response.data.data;
+        // console.log(nextPageresult);
+
+     res.render("index.ejs", {
+         year: date.getFullYear(),
+         fact1 : nextPageresult[0].fact,
+         fact2 : nextPageresult[1].fact,
+         fact3 : nextPageresult[2].fact,
+         fact4 : nextPageresult[3].fact,
+         fact5 : nextPageresult[4].fact,
+     });
+
+
+ } catch(error){
+    console.log(error);
+     }
+
+})
+
+
+app.get("/about", (req, res) =>{
+    res.render("about.ejs", {
+        year: date.getFullYear(),
+    });
+})
+
+
+app.listen(port, () => {
+    console.log(`listening to the port ${port}`);
+})
